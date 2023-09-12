0d61431c944b446850403dbbf7987aaf81b60f85
Kia Karimi <kiakarimi82@gmail.com>
2023-09-11 20:23:23 -0700

    improved code readability by putting relatively similar code into a function call and adjusted the first 3 header titles to become more human readiable

--- dynamic/dynamic.py
+++ dynamic/dynamic.py
@@ -2,37 +2,44 @@ import requests
 import pandas as pd
 
 
-temp_season = ['input 0: 2022-23', 'input 1: 2021-22','input 2: 2020-21']
-seasons = ['2022-23', '2021-22', '2020-21'] # -> url list, don't modify
+def take_input():
+    print(">>>", end=" ")
+    return int(int(input()))
+
+
+temp_season = ['input 0: 2022-23', 'input 1: 2021-22', 'input 2: 2020-21']
+seasons = ['2022-23', '2021-22', '2020-21']  # -> url list, don't modify
 
 print('select season: {}'.format(temp_season))
-print(">>>", end = " ")
-input_s = int(input())
-s = seasons[input_s]
 
-seasons = ['2022-23', '2021-22', '2020-21'] # -> list can modify
+s_input = take_input()
+s = seasons[s_input]
 
-temp_season_types = ['input 0: Regular Season', 'input 1: Pre Season','input 2: Playoffs']
-season_types = ['Regular%20Season','Pre+Season','Playoffs'] # -> url list, don't modify
+temp_season_types = ['input 0: Regular Season', 'input 1: Pre Season', 'input 2: Playoffs']
+season_types = ['Regular%20Season', 'Pre+Season', 'Playoffs']  # -> url list, don't modify
 
 print('select season types: {}'.format(temp_season_types))
-print(">>>", end = " ")
-input_st = int(input())
-st = season_types[input_st]
 
-season_types = ['Regular Season','Pre Season','Playoffs'] # -> list can modify
+st_input = take_input()
+st = season_types[st_input]
 
 temp_per_mode = ['input 0: PerGame ', 'input 1: Totals', 'input 2: Per 48 Minutes']
-per_mode = ['PerGame ', 'Totals', 'Per48'] # -> url list, don't modify
+per_mode = ['PerGame ', 'Totals', 'Per48']  # -> url list, don't modify
 
 print('select Mode: {}'.format(temp_per_mode))
-print(">>>", end = " ")
-input_m = input()
-m = per_mode[int(input_m)]
 
-per_mode = ['PerGame ', 'Totals', 'Per 48 Min'] # -> list can modify
+m_input = take_input()
+m = per_mode[m_input]
 
 url = "https://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=" + m + "&Scope=S&Season=" + s + "&SeasonType=" + st + "&StatCategory=PTS"
+
+seasons = ['2022-23', '2021-22', '2020-21']  # -> list can modify
+season_types = ['Regular Season', 'Pre Season', 'Playoffs']  # -> list can modify
+per_mode = ['PerGame ', 'Totals', 'Per 48 Min']  # -> list can modify
+
+s = seasons[s_input]
+st = season_types[st_input]
+m = per_mode[m_input]
 
 headers = {
     'authority': 'stats.nba.com',
@@ -67,9 +74,9 @@ temp_df1 = pd.DataFrame(playerData, columns=categories, index=index)
 temp_df1 = temp_df1.drop("TEAM_ID", axis=1)
 
 temp_df2 = pd.DataFrame({
-    "SEASON": [s  for i in range(size)],
-    "SEASON TYPE": [st  for i in range(size)],
-    "PER MODE": [m  for i in range(size)],
+    "SEASON": [s for i in range(size)],
+    "SEASON TYPE": [st for i in range(size)],
+    "PER MODE": [m for i in range(size)],
     "STAT CATEGORY": ['PTS' for i in range(size)]
 }, index=index)
 
